517522e54f51186ff3cabdf4e8a6a16c8cad9ac5
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2024-06-10 21:44:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask, request, jsonify, render_template
 import requests
 from PyPDF2 import PdfReader, errors
@@ -164,4 +165,4 @@ def save_results():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True) 
+    app.run(host='0.0.0.0', port=8000, debug=True)
